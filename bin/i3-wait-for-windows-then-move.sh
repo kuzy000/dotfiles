2ada97a7cc07e4a0b17dfcbe3cd843d10eede709
Egor Moiseenko <kuzy000@gmail.com>
2016-03-21 17:26:33 +0300

    Up

--- bin/i3-wait-for-windows-then-move.sh
+++ bin/i3-wait-for-windows-then-move.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 while : ; do
-	pid=`pidof $1`
+	pid=`pidof $1 | sed -e 's/ /\n/g'`
 	expected=$2
-	actual=`wmctrl -lp | awk '{ print $3 }' | grep -o "$pid" | wc -l`
+	actual=`wmctrl -lp | awk '{ print $3 }' | fgrep -o "$pid" | wc -l`
 	[[ $expected -gt $actual ]] || break
 done
 
-windowIds=`wmctrl -lp | awk '{ print $1" "$3 }' | sed -e 's/\(0x[0-9a-f]\{8\}\) '$pid'/\1/;tx;d;:x'`
+for i in $pid; do
+	windowIds=`wmctrl -lp | awk '{ print $1" "$3 }' | sed -e 's/\(0x[0-9a-f]\{8\}\) '$i'/\1/;tx;d;:x'`
 
-echo "$windowIds" | xargs -i i3-msg '[id="{}"] move container to workspace '$3
+	echo "$windowIds" | xargs -i i3-msg '[id="{}"] move container to workspace '$3
+done
 
